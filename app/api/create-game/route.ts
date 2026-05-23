40ca5dc7a9870725e0cf2d73e9ff05802438f913
Andrew Lau <andrewlau2019@gmail.com>
2026-05-22 19:17:07 -0700

    Moved client instantiation

--- app/api/create-game/route.ts
+++ app/api/create-game/route.ts
@@ -1,11 +1,6 @@
 // app/api/create-game/route.ts
 import { NextResponse } from "next/server";
-import { createClient } from "@supabase/supabase-js";
-
-// Initialize Supabase client for the server
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseServiceKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!; // Use service role for backend insertion
-const supabase = createClient(supabaseUrl, supabaseServiceKey);
+import { supabaseServer } from "@/lib/supabase/client";
 
 const generateRoomCode = () => {
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -20,7 +15,7 @@ export async function POST() {
   const inviteCode = generateRoomCode();
 
   // Insert the new game into the games table
-  const { data, error } = await supabase
+  const { data, error } = await supabaseServer
     .from("games")
     .insert([
       {
